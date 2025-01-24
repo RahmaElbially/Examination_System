14f71d9d1d8ec6aea2db6c3691bd09b2ac781975
WA7iD <rahmamohammed117@gmail.com>
2025-01-24 17:00:31 +0200

    edit in styles

--- JS/timeOut.js
+++ JS/timeOut.js
@@ -2,4 +2,4 @@ let names =document.querySelector(".timeUp")
 let fname=localStorage.getItem("firstName")
 let lname=localStorage.getItem("lastName")
 // console.log(name);
-names.textContent=`sorry ${fname} ${lname} , timeout!!!`
+names.textContent=`sorry ${fname} ${lname} , timeout !`
