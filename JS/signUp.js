4eb25c58ccdf394be4d6f91f00968d7c6a297e0d
WA7iD <rahmamohammed117@gmail.com>
2025-01-25 09:10:19 +0200

    edit exising user in signup

--- JS/signUp.js
+++ JS/signUp.js
@@ -82,15 +82,9 @@ function namesReg(e){
 function emailReg(){
     let emailValue = emailInput.value; 
     let isEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(emailValue);
-    let users = JSON.parse(localStorage.getItem("users")) || []; 
-    let isUserExist = users.some(user => user.email === emailValue);
     if(!isEmail){
         emailError.style.visibility = "visible";
         emailError.textContent = "This Is Invalid Email";
-        emailInput.style.boxShadow = "0px 0px 10px #f00";
-    }else if(isUserExist){
-        emailError.style.visibility = "visible";
-        emailError.textContent = "This User Is Already Exist";
         emailInput.style.boxShadow = "0px 0px 10px #f00";
     }else{
         emailError.style.visibility = "hidden";
@@ -123,9 +117,17 @@ function lastNameReq(){
 }
 
 function emailReq(){
+    let emailValue = emailInput.value; 
+    let users = JSON.parse(localStorage.getItem("users")) || []; 
+    let isUserExist = users.some(user => user.email === emailValue);
+
     if(emailInput.value === ""){
         emailError.style.visibility = "visible";
         emailError.textContent = "This Field Is Required";
+        emailInput.style.boxShadow = "0px 0px 10px #f00";
+    }else if(isUserExist){
+        emailError.style.visibility = "visible";
+        emailError.textContent = "This User Is Already Exist";
         emailInput.style.boxShadow = "0px 0px 10px #f00";
     }else{
         emailError.style.visibility = "hidden";
