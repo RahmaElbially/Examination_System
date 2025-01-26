8cd4d45bf52ce461aa596730b1f3d34e971107f9
samaahmedmohameds <sa6814620@gmail.com>
2025-01-26 16:31:47 +0200

    graderateLogin

--- JS/grade.js
+++ JS/grade.js
@@ -5,30 +5,139 @@
 // // console.log(name);
 // names.textContent=`Congratlations ${fname} ${lname}`
 // namef.textContent=`sorry you are failed ${fname} ${lname}`
-let image = document.querySelector(".success img");
-let names = document.querySelector(".con");
-let namef = document.querySelector(".fail");
+/////////////////////////////////////////////////////////////////////
+// let image = document.querySelector(".success img");
+// let names = document.querySelector(".con");
+// let namef = document.querySelector(".fail");
 
-let users = JSON.parse(localStorage.getItem("users")) || [];
-const currentUserEmail = JSON.parse(localStorage.getItem("currentUser")).email;
-const currentUser = users.find(user => user.email === currentUserEmail);
+// let users = JSON.parse(localStorage.getItem("users")) || [];
+// const currentUserEmail = JSON.parse(localStorage.getItem("currentUser")).email;
+// const currentUser = users.find(user => user.email === currentUserEmail);
 
-if (currentUser) {
-    const fname = currentUser.firstName || 'First Name'; 
-    const lname = currentUser.lastName || 'Last Name';  
-    const correctdAnswers = currentUser.correctAnswers || 0; 
-    const inCorrectdAnswers = currentUser.incorrectAnswers || 0;   
+// if (currentUser) {
+//     const fname = currentUser.firstName || 'First Name'; 
+//     const lname = currentUser.lastName || 'Last Name';  
+//     const correctdAnswers = currentUser.correctAnswers || 0; 
+//     const inCorrectdAnswers = currentUser.incorrectAnswers || 0;   
+//     /////////new////////
+//     const successRate=(correctdAnswers/10)*100
+//     const failRate=(correctdAnswers/10)*100
 
-    let resultMessage = '';
-    if (correctdAnswers >= inCorrectdAnswers) {
-        resultMessage = `Congratulations ${fname} ${lname}, you passed with ${correctdAnswers} correct answers!`;
-        names.textContent = resultMessage;
-        image.src = "../Images/Success_factors.gif"; 
+//     let resultMessage = '';
+//     if (correctdAnswers >= inCorrectdAnswers) {
+//         resultMessage = `Congratulations ${fname} ${lname}, you passed with ${successRate}% correct answers!`;
+//         names.textContent = resultMessage;
+//         image.src = "../Images/Success_factors.gif"; 
+//     } else {
+//         resultMessage = `Sorry ${fname} ${lname}, you failed with ${failRate}% wrong answers.`;
+//         namef.textContent = resultMessage;
+//         image.src = "../Images/Worried.gif"; 
+//     }
+// } else {
+//     console.log("No user data found");
+// }
+
+
+// let circleContainer = document.querySelector(".circle-container");
+//         let percentageText = document.querySelector(".percentage-text")
+//         let input = document.querySelector("input");
+
+//         input.addEventListener("blur", () => {
+//             let percentage = parseInt(input.value);
+
+//             // تأكد أن القيمة بين 0 و 100
+//             if (isNaN(percentage) || percentage < 0 || percentage > 100) {
+//                 alert("Please enter a valid percentage between 0 and 100");
+//                 return;
+//             }
+//         let currentPercentage = parseFloat(getComputedStyle(circleContainer).getPropertyValue("--percentage")) || 0;
+//             let step = 1; // الزيادة لكل خطوة
+//             let interval = 10; // سرعة التحديث بالميلي ثانية
+//             let animation = setInterval(() => {
+//                 if (currentPercentage < percentage) {
+//                     currentPercentage += step;
+//                 } else if (currentPercentage > percentage) {
+//                     currentPercentage -= step;
+//                 } else {
+//                     clearInterval(animation);
+//                 }
+//                 circleContainer.style.setProperty("--percentage", currentPercentage);
+//                 percentageText.textContent =`${Math.round(currentPercentage)}%` ;
+//             }, interval);
+//         });
+
+async function fetchData(){
+    const response=await fetch("questions.json")
+    const data =await response.json();
+    console.log(data.questions.length);
+      
+    const lengthOfData=data.questions.length
+
+    let circleContainer = document.querySelector(".circle-container");
+    let percentageText = document.querySelector(".percentage-text");
+    let image = document.querySelector(".success img");
+    let names = document.querySelector(".con");
+    let namef = document.querySelector(".fail");
+    let res=document.querySelector(".res")
+    
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    const currentUserEmail = JSON.parse(localStorage.getItem("currentUser")).email;
+    const currentUser = users.find(user => user.email === currentUserEmail);
+    
+    if (currentUser) {
+       const fname = currentUser.firstName || 'First Name'; 
+       const lname = currentUser.lastName || 'Last Name';  
+       const correctAnswers = currentUser.correctAnswers || 0; 
+       const incorrectAnswers = currentUser.incorrectAnswers || 0;   
+    
+       ///////// نسبة النجاح والفشل /////////
+       const successRate = (correctAnswers / lengthOfData) * 100;
+    //    const failRate = (incorrectAnswers / lengthOfData) * 100;
+    
+       let resultMessage = '';
+       let resultMessage2= '';
+       let percentageToAnimate = 0; // المتغير لتحديد النسبة التي سيتم عرضها
+    
+       if (correctAnswers >= incorrectAnswers) {
+           resultMessage = `Congratulations ${fname} ${lname},`;
+           resultMessage2=` you passed with ${correctAnswers} correct answers!`
+           names.textContent = resultMessage;
+           res.textContent=resultMessage2;
+           image.src = "../Images/Success_factors.gif"; 
+           percentageToAnimate = successRate; // استخدم نسبة النجاح
+       } else {
+           resultMessage = `Sorry ${fname} ${lname},`;
+           resultMessage2=` you failed with ${incorrectAnswers} wrong answers.`
+           namef.textContent = resultMessage;
+           res.textContent=resultMessage2;
+           image.src = "../Images/Worried.gif"; 
+           percentageToAnimate = successRate; // استخدم نسبة الفشل
+       }
+    
+       // تحديث الدائرة بالنسبة المحسوبة تلقائيًا
+       animatePercentage(percentageToAnimate);
     } else {
-        resultMessage = `Sorry ${fname} ${lname}, you failed with ${inCorrectdAnswers} wrong answers.`;
-        namef.textContent = resultMessage;
-        image.src = "../Images/Worried.gif"; 
+       console.log("No user data found");
     }
-} else {
-    console.log("No user data found");
+    
+    /////////// وظيفة لتحديث الدائرة ديناميكيًا ///////////
+    function animatePercentage(targetPercentage) {
+       let currentPercentage = parseFloat(getComputedStyle(circleContainer).getPropertyValue("--percentage")) || 0;
+       let step = 1; // الزيادة لكل خطوة
+       let interval = 10; // سرعة التحديث بالميلي ثانية
+    
+       let animation = setInterval(() => {
+           if (currentPercentage < targetPercentage) {
+               currentPercentage += step;
+           } else if (currentPercentage > targetPercentage) {
+               currentPercentage -= step;
+           } else {
+               clearInterval(animation);
+           }
+           circleContainer.style.setProperty("--percentage", currentPercentage);
+           percentageText.textContent = `${Math.round(currentPercentage)}%`;
+       }, interval);
+    }
+    console.log();
 }
+fetchData()
--- JS/signIn.js
+++ JS/signIn.js
@@ -6,40 +6,36 @@ let emailErr=document.querySelector(".emailErr")
 let passErr=document.querySelector(".passErr")
 let subErr=document.querySelector(".subErr")
 
+let eyeIcon =document.querySelector(".pas i")
+
 const users = JSON.parse(localStorage.getItem("users")) || [];
 console.log(users)
 
 login.addEventListener("click" , function(e){
     e.preventDefault();
+    emailCheck();
+    passCheck();
     let checked =true;
     if(loginEmail.value==""){
         emailErr.textContent="Required";
         checked=false;
         loginEmail.style.boxShadow="0px 0px 10px #f00"
-    }else if(!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(loginEmail.value)){
-        emailErr.textContent="please enter valid email"
-        checked=false;
-        loginEmail.style.boxShadow="0px 0px 10px #f00"
+        eyeIcon.style.lineHeight="1"
     }
     else{
         emailErr.textContent="";
+        loginEmail.style.boxShadow="1px 1px 10px #aaa"
     }
 
-    const  regExpPass=/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/
-
+    
     if(loginPass.value==""){
         passErr.textContent="Required"
         checked=false;
         loginPass.style.boxShadow="0px 0px 10px #f00"
+        eyeIcon.style.lineHeight="1"
     }else {
-        if(!(loginPass.value.length>=8) ){
-            passErr.textContent="please enter 8 digit"
-            checked=false;
-            loginPass.style.boxShadow="0px 0px 10px #f00"
-        }
-        else{
             passErr.textContent="";
-        }
+            loginPass.style.boxShadow="1px 1px 10px #aaa"
     }
 
     if(checked){
@@ -54,16 +50,49 @@ login.addEventListener("click" , function(e){
                 } else {
                     passErr.textContent = "Wrong password";
                     loginPass.style.boxShadow="0px 0px 10px #f00";
+                    eyeIcon.style.lineHeight="1"
                     break;
                 }
+            }else{
+                emailErr.textContent = "Wrong Email";
+             loginEmail.style.boxShadow="0px 0px 10px #f00";
             }
         }
-        if (!userFound) {
-            emailErr.textContent = "Wrong Email";
-            loginEmail.style.boxShadow="0px 0px 10px #f00";
-        }
+        // if (!userFound) {
+        //     // emailErr.textContent = "Wrong Email";
+        //     // loginEmail.style.boxShadow="0px 0px 10px #f00";
+        // }
     }
 })
+
+function emailCheck(){
+    loginEmail.addEventListener("input" , function(){
+        if(!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(loginEmail.value)){
+          emailErr.textContent="please enter valid email"
+          checked=false;
+          loginEmail.style.boxShadow="0px 0px 10px #f00"
+          eyeIcon.style.lineHeight="1"
+      }else{
+        emailErr.textContent="";
+        loginEmail.style.boxShadow="1px 1px 10px #aaa"
+    }
+    })
+}
+function passCheck(){
+const  regExpPass=/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/
+    loginPass.querySelector("input",function(){
+            if(!(loginPass.value.length>=8) ){
+                passErr.textContent="please enter 8 digit"
+                checked=false;
+                loginPass.style.boxShadow="0px 0px 10px #f00"
+                eyeIcon.style.lineHeight="1"
+            }
+            else{
+                passErr.textContent="";
+                loginPass.style.boxShadow="1px 1px 10px #aaa"
+            }
+    })
+}
 
 
 const togglePassword = document.getElementById("togglePassword");
